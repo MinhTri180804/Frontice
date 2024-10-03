3171f3ce76f9430817d9b6c7e59f99ab7bf71752
ngothanhy-testmail <ngothanhy65.ct.ct@gmail.com>
2024-10-03 23:24:28 +0700

    repactor(comment): remove comment

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,9 +1,11 @@
 import './Button.scss';
+
 interface ButtonProps {
   size?: 'extra-small' | 'small' | 'medium' | 'normal' | 'large';
   iconPosition?: 'left' | 'right';
   label?: string; // Nhãn của button
   type?: 'primary' | 'secondary' | 'tertiary';
+  Icon?: React.FC<React.SVGProps<SVGSVGElement>>;
 }
 
 const Button: React.FC<ButtonProps> = ({ ...props }) => {
