86f49b91b046659cd21f26a5219a74c16573dd17
Minh Tri <nguyenminhtri1808t@gmail.com>
2024-10-03 22:24:35 +0700

    refactor: refactor code testing when init scss

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -2,7 +2,6 @@ const Home: React.FC = () => {
   return (
     <div className="home-container">
       <h1>Home Page</h1>
-      <h2 className="testing">Xin chào các bạn mình là trí nè</h2>
     </div>
   );
 };
