6aaf5781a25f15c9ed0950bdbbace0901e04833b
ngothanhy-testmail <ngothanhy65.ct.ct@gmail.com>
2024-10-03 22:16:51 +0700

    feat: component button

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,9 +1,36 @@
+import Button from '../../components/common/Button/Button';
+
 const Home: React.FC = () => {
-    return (
-        <div className="home-container">
-            <h1>Home Page</h1>
-        </div>
-    );
+  return (
+    <div className="home-container">
+      <h1>Home Page</h1>
+      <Button
+        type="secondary"
+        size="large"
+        label="button-large"
+        iconPosition="left"
+      />
+      <Button
+        type="primary"
+        size="normal"
+        label="button-normal"
+        iconPosition="right"
+      />
+      <Button
+        type="tertiary"
+        size="medium"
+        label="button-medium"
+        iconPosition="left"
+      />
+      <Button size="small" label="button-small" iconPosition="left" />
+      <Button
+        type="primary"
+        size="extra-small"
+        label="button-extra-small"
+        iconPosition="left"
+      />
+    </div>
+  );
 };
 
 export default Home;
