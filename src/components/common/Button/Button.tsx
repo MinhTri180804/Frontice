809803d0a32de2958854e065e8006cce8b1cd1af
ngothanhy-testmail <ngothanhy65.ct.ct@gmail.com>
2024-10-04 01:17:27 +0700

    feat: [FE-04] update button

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,53 +1,27 @@
 import './Button.scss';
 
 interface ButtonProps {
-  size?: 'extra-small' | 'small' | 'medium' | 'normal' | 'large';
+  size: 'extra-small' | 'small' | 'medium' | 'normal' | 'large';
   iconPosition?: 'left' | 'right';
-  label?: string; // Nhãn của button
-  type?: 'primary' | 'secondary' | 'tertiary';
+  label: string; // Nhãn của button
+  type: 'primary' | 'secondary' | 'tertiary';
   Icon?: React.FC<React.SVGProps<SVGSVGElement>>;
 }
 
 const Button: React.FC<ButtonProps> = ({ ...props }) => {
-  const { label, size, iconPosition, type } = props;
+  const { label, size, iconPosition, type, Icon } = props;
 
   return (
     <button className={`button-container  ${size} ${type}`}>
-      {iconPosition == 'left' && (
+      {iconPosition == 'left' && Icon && (
         <div className="icon">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-            />
-          </svg>
+          <Icon />
         </div>
       )}
-      <label>{label}</label>
-      {iconPosition == 'right' && (
+      <div>{label}</div>
+      {iconPosition == 'right' && Icon && (
         <div className="icon">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-            />
-          </svg>
+          <Icon />
         </div>
       )}
     </button>
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,5 +1,5 @@
 import Button from '../../components/common/Button/Button';
-
+import { ArrowLongRightIcon } from '@heroicons/react/24/solid';
 const Home: React.FC = () => {
   return (
     <div className="home-container">
@@ -9,25 +9,35 @@ const Home: React.FC = () => {
         size="large"
         label="button-large"
         iconPosition="left"
+        Icon={() => <ArrowLongRightIcon />}
       />
       <Button
         type="primary"
         size="normal"
         label="button-normal"
         iconPosition="right"
+        Icon={() => <ArrowLongRightIcon />}
       />
       <Button
         type="tertiary"
         size="medium"
         label="button-medium"
         iconPosition="left"
+        Icon={() => <ArrowLongRightIcon />}
       />
-      <Button size="small" label="button-small" iconPosition="left" />
+      <Button
+        type="tertiary"
+        size="small"
+        label="button-small"
+        iconPosition="left"
+        Icon={() => <ArrowLongRightIcon />}
+      />
       <Button
         type="primary"
         size="extra-small"
         label="button-extra-small"
         iconPosition="left"
+        Icon={() => <ArrowLongRightIcon />}
       />
     </div>
   );
