148d5046000738dc9bb6c01cdcf345f078da7b85
Minh Tri <nguyenminhtri1808t@gmail.com>
2024-10-04 11:11:45 +0700

    refactor(component): modified scss in input component

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,34 +1,40 @@
-import React from 'react';
-import './Input.scss'
+import React, { useId } from 'react';
+import './Input.scss';
 
 interface InputProps {
-    label?: string;
-    size?: 'small' | 'medium' | 'large';
-    type: string;
-    placeholder?: string;
-    required?: boolean;
-    Icon?: React.FC<React.SVGProps<SVGSVGElement>>;
+  label?: string;
+  size?: 'small' | 'medium' | 'large';
+  type: string;
+  placeholder?: string;
+  required?: boolean;
+  Icon?: React.FC<React.SVGProps<SVGSVGElement>>;
 }
 
 const Input: React.FC<InputProps> = ({ ...props }) => {
+  const idInput = useId();
 
-    const { label, type, placeholder, required, size, Icon } = props
+  const { label, type, placeholder, required, size, Icon } = props;
 
-    return (
-        <div className={`input-container ${size}`}>
-            {label &&
-                <label>
-                    {label} {required && <span style={{ color: 'red' }}>*</span>}
-                </label>
-            }
-            <input type={type} placeholder={placeholder} required={required} />
-            {Icon &&
-                <div className="icon">
-                    <Icon />
-                </div>
-            }
+  return (
+    <div className={`input-container ${size}`}>
+      {label && (
+        <label htmlFor={idInput}>
+          {label} {required && <span style={{ color: 'red' }}>*</span>}
+        </label>
+      )}
+      <input
+        id={idInput}
+        type={type}
+        placeholder={placeholder}
+        required={required}
+      />
+      {Icon && (
+        <div className="icon">
+          <Icon />
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
-export default Input
+export default Input;
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,7 +1,16 @@
+import { Input } from '../../components/common';
+
 const Home: React.FC = () => {
   return (
     <div className="home-container">
       <h1>Home Page</h1>
+      <div
+        style={{
+          width: '120px',
+        }}
+      >
+        <Input type="text" />
+      </div>
     </div>
   );
 };
