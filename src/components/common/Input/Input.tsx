2a44b1b754bdd61416cac442685a0a7fb866a952
duythong114 <tranthong.facebook@gmail.com>
2024-10-04 10:59:50 +0700

    update Input component

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -2,25 +2,31 @@ import React from 'react';
 import './Input.scss'
 
 interface InputProps {
-    title?: string;
-    width?: 'small' | 'medium' | 'large';
+    label?: string;
+    size?: 'small' | 'medium' | 'large';
     type: string;
     placeholder?: string;
     required?: boolean;
+    Icon?: React.FC<React.SVGProps<SVGSVGElement>>;
 }
 
 const Input: React.FC<InputProps> = ({ ...props }) => {
 
-    const { title, type, placeholder, required, width } = props
+    const { label, type, placeholder, required, size, Icon } = props
 
     return (
-        <div className={`input-container ${width}`}>
-            {title &&
+        <div className={`input-container ${size}`}>
+            {label &&
                 <label>
-                    {title} {required && <span style={{ color: 'red' }}>*</span>}
+                    {label} {required && <span style={{ color: 'red' }}>*</span>}
                 </label>
             }
             <input type={type} placeholder={placeholder} required={required} />
+            {Icon &&
+                <div className="icon">
+                    <Icon />
+                </div>
+            }
         </div>
     );
 };
