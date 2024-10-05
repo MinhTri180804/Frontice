91d210f22af36d58e7e6f5c7e0bc3235219c3a14
Minh Tri <nguyenminhtri1808t@gmail.com>
2024-10-05 15:52:07 +0700

    feat(component): [FE-05-stepProcess-component] completed component stepProcess

--- src/pages/Auth/Partials/StepProcess/StepProcess.tsx
+++ src/pages/Auth/Partials/StepProcess/StepProcess.tsx
@@ -1,15 +1,34 @@
 import './stepProcess.scss';
 import { FC } from 'react';
+import classNames from 'classnames';
 
 interface IStepProcessProps {
-  mock: string;
+  stepNumber: string | number;
+  stepContent: string;
+  isActive: boolean;
+  eventClick: () => void;
 }
 
-const StepProcess: FC<IStepProcessProps> = ({ mock }) => {
-  console.log(mock);
+const StepProcess: FC<IStepProcessProps> = ({
+  stepContent,
+  stepNumber,
+  isActive,
+  eventClick,
+}) => {
+  // Define className condition for component
+  const stepProcessClass = classNames({
+    'step__process-component': true,
+    active: isActive,
+  });
+
+  const handleClick: () => void = () => {
+    eventClick();
+  };
+
   return (
-    <div>
-      <div>This is step process</div>
+    <div className={stepProcessClass} onClick={handleClick}>
+      <div className="step-number">{stepNumber}</div>
+      <div className="step-content">{stepContent}</div>
     </div>
   );
 };
