8967d1eec8b5e1af1b96596a1885614a6aa84dda
Minh Tri <nguyenminhtri1808t@gmail.com>
2024-10-06 13:55:42 +0700

    feat(page): init structure otp page

---
+++ src/pages/Auth/OTP/OtpPage.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import './otpPage.scss';
+
+const OtpPage: FC = () => {
+  return (
+    <div>
+      <div>This is otp page</div>
+    </div>
+  );
+};
+
+export default OtpPage;
