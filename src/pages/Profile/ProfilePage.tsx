752b25f52ab6e39a7af62fcbdcd36775c36c8aa9
ngothanhy-testmail <ngothanhy65.ct.ct@gmail.com>
2024-10-07 22:16:51 +0700

    feat(profile) [FE-13-create-profile-page] update component solution

--- src/pages/Profile/ProfilePage.tsx
+++ src/pages/Profile/ProfilePage.tsx
@@ -1,21 +1,51 @@
+import React, { useEffect, useState } from 'react';
 import BannerWithInfo from './Partials/BannerWithInfo';
 import Solution from './Partials/Solution';
 import image from '../../asset/images/solution.png';
 import './ProfilePage.scss';
 
+interface DataItem {
+  time: string;
+  name: string;
+  tech: string[];
+  id: string;
+} // TODO: implement
+
 const Profile: React.FC = () => {
-  const tech = ['html', 'css', 'js'];
+  const [dataSolution, setDataSolution] = useState<DataItem[]>([]);
+
+  //  async/await lấy dữ liệu từ file data.json
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/data.json'); //duong dan file data.json
+        const result = await response.json();
+        setDataSolution(result); // Lưu dữ liệu vào resutl state==>o ham [data,setData]
+      } catch (error) {
+        console.error('Error', error); // Xử lý lỗi
+      } finally {
+        // Sau khi tải xong thì tắt trạng thái loading
+      }
+    };
+
+    fetchData(); // Gọi hàm fetchData
+  }, []); // Chạy khi component mount ==> tim hieu
+
   return (
     <div className="profile-container">
-      <h1>Profile Page</h1>
+      <h4>Profile Page</h4>
       <BannerWithInfo />
       <div className="list-solution">
-        <Solution
-          image={image}
-          name="Bento grid"
-          time="Submitted about 1 hour ago"
-          tech={tech}
-        />
+        {/* ham map looop qua tung solution */}
+        {dataSolution.map((solutionItem) => (
+          <Solution
+            key={solutionItem.id}
+            image={image} // Hình ảnh mặc định
+            name={solutionItem.name}
+            time={solutionItem.time}
+            tech={solutionItem.tech}
+          />
+        ))}
       </div>
     </div>
   );
