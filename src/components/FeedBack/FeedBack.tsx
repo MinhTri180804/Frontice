b12eb2992467b9b3f6720946f4a71b4cd74721b6
ngothanhy-testmail <ngothanhy65.ct.ct@gmail.com>
2024-10-09 14:05:43 +0700

    refactor(filterWrapperComponent) [FE-25-filter-wrapper-component] simple layout - Status: Done

---
+++ src/components/FeedBack/FeedBack.tsx
@@ -0,0 +1,12 @@
+const FeedBack: React.FC = () => {
+  return (
+    <>
+      <div className="container-feedback">
+        <div className="title">FeebBack</div>
+        <div className="input"></div>
+      </div>
+    </>
+  );
+};
+
+export default FeedBack;
