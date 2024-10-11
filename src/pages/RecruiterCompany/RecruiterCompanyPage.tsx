56f975342cd2f5f1a8a284467db5bd27a55b8477
duythong114 <tranthong.facebook@gmail.com>
2024-10-11 09:30:13 +0700

    feat(component): [FE-36-recruiter-profile] completed recruiter-profile ui

--- src/pages/RecruiterCompany/RecruiterCompanyPage.tsx
+++ src/pages/RecruiterCompany/RecruiterCompanyPage.tsx
@@ -8,14 +8,19 @@ import {
 const RecruiterCompanyPage: React.FC = () => {
   return (
     <div className="recruiter-company-container">
-      <RecruiterProfile/>
-      <RecruiterInfo/>
-      <RecruiterDescription/>
-      <div className='preview-component'>
-        <h1>Preview Component</h1>
+      <h1 className='recruiter-company-title'>Recruiter Company</h1>
+      <RecruiterProfile />
+      <div className="recruiter-company-content">
+        <div className="left-column">
+          <RecruiterInfo />
+          <RecruiterDescription />
+        </div>
+        <div className="right-column">
+        </div>
       </div>
       <div className='challenge-list-component'>
-        <h1>Challenge List</h1>
+        <h2>Challenges List</h2>
+        {/* Add challenge list content here */}
       </div>
     </div>
   );
--- src/pages/RecruiterCompany/partials/RecruiterProfile/RecruiterProfile.tsx
+++ src/pages/RecruiterCompany/partials/RecruiterProfile/RecruiterProfile.tsx
@@ -1,9 +1,37 @@
 import './RecruiterProfile.scss';
+import { RecruiterLogo } from '../../../../assets/images';
+import { ListIcon, AddressIcon, PlusIcon } from '../../../../assets/icons';
+import { Button } from '../../../../components/common';
 
 const RecruiterProfile: React.FC = () => {
   return (
     <div className="recruiter-profile-container">
-      <h1>RecruiterProfile component</h1>
+      <img className='left-content' src={RecruiterLogo} alt="recruiter-logo" />
+      <div className='right-content'>
+        <div className='recruiter-profile-info'>
+          <div className='recruiter-name'>
+            NAB Innovation Centre Vietnam
+          </div>
+          <div className='recruiter-detail'>
+            <div className='recruiter-address'>
+              <AddressIcon width={24} height={24} stroke='#fff' />
+              <span>Thành phố Hồ Chí Minh, Việt Nam</span>
+            </div>
+            <div className='recruiter-challenge-numbers'>
+              <ListIcon width={24} height={24} stroke='#fff' />
+              <span>Có 11 challenge</span>
+            </div>
+          </div>
+        </div>
+        <div className='recruiter-btn-container'>
+          <div className='recruiter-btn'>
+            <Button label='Challenges' type='secondary' size='medium' />
+          </div>
+          <div className='recruiter-btn active'>
+            <Button label='Theo dõi' type='secondary' size='medium' Icon={PlusIcon} iconPosition='left' />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
