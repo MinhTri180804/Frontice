7175be706f730b0499fb06c2af91fbd6e4d97c02
Minh Tri <nguyenminhtri1808t@gmail.com>
2024-10-12 15:44:53 +0700

    refactor(component): refactor flex style of icon in button and label in button

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -6,13 +6,17 @@ interface ButtonProps {
   label: string; // Nhãn của button
   type: 'primary' | 'secondary' | 'tertiary';
   Icon?: React.FC<React.SVGProps<SVGSVGElement>>;
+  isDisabled?: boolean;
 }
 
 const Button: React.FC<ButtonProps> = ({ ...props }) => {
-  const { label, size, iconPosition, type, Icon } = props;
+  const { label, size, iconPosition, type, Icon, isDisabled = false } = props;
 
   return (
-    <button className={`button-container  ${size} ${type}`}>
+    <button
+      className={`button-container  ${size} ${type}`}
+      disabled={isDisabled}
+    >
       {iconPosition == 'left' && Icon && (
         <div className="icon">
           <Icon />
