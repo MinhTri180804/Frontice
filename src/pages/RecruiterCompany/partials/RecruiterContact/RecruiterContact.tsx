a3c4619789582162cf572ea93f540f69e55ea705
Minh Tri <nguyenminhtri1808t@gmail.com>
2024-10-12 18:51:38 +0700

    refactor(component): separated recruiterContactItem to component

--- src/pages/RecruiterCompany/partials/RecruiterContact/RecruiterContact.tsx
+++ src/pages/RecruiterCompany/partials/RecruiterContact/RecruiterContact.tsx
@@ -1,51 +1,34 @@
+import { RecruiterContactItem } from './Partials';
 import './RecruiterContact.scss';
 
 const RecruiterContact: React.FC = () => {
   return (
     <div className="recruiter-contact-container">
       <div className="recruiter-contact-list">
-        <div className="contact-item">
-          <div className="contact-label">
-            <span>Website</span>
-          </div>
-          <div className="contact-value">
-            <a
-              href="https://k-tech.net.vn"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              https://k-tech.net.vn
-            </a>
-          </div>
-        </div>
-        <div className="contact-item">
-          <div className="contact-label">
-            <span className="contact-label">LinkedIn</span>
-          </div>
-          <div className="contact-value">
-            <a
-              href="https://linkedin.com/k-tech-vn"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              https://linkedin.com/k-tech-vn
-            </a>
-          </div>
-        </div>
-        <div className="contact-item">
-          <div className="contact-label">
-            <span className="contact-label">Facebook</span>
-          </div>
-          <div className="contact-value">
-            <a
-              href="https://facebook.com/k-tech-vn"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              https://facebook.com/k-tech-vn
-            </a>
-          </div>
-        </div>
+        <RecruiterContactItem
+          contactLabel="Website"
+          contactValue={{
+            content: 'https://k-tech.vn',
+            to: 'https://k-tech.vn',
+            blank: true,
+          }}
+        />
+        <RecruiterContactItem
+          contactLabel="Linkedin"
+          contactValue={{
+            content: 'https://linkedin.com/k-tech-vn',
+            to: 'https://linkedin.com/k-tech-vn',
+            blank: true,
+          }}
+        />
+        <RecruiterContactItem
+          contactLabel="Facebook"
+          contactValue={{
+            content: 'https://facebook.com/k-tech-vn',
+            to: 'https://facebook.com/k-tech-vn',
+            blank: true,
+          }}
+        />
       </div>
     </div>
   );
