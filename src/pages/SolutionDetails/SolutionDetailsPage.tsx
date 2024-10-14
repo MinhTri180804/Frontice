75de34e716f86474642a620d66247b3f45d9aff9
ngothanhy <ngothanhy54.ct.ct@gmail.com>
2024-10-14 11:51:55 +0700

    [FE-39-solution-detail-component] updating solution detailpage

--- src/pages/SolutionDetails/SolutionDetailsPage.tsx
+++ src/pages/SolutionDetails/SolutionDetailsPage.tsx
@@ -1,13 +1,15 @@
+import { ChallengeOverview } from '../../components/common';
 import Action from './Partials/Action/Action';
 import SolutitonDetails from './Partials/Details';
 import Feedback from './Partials/Feedback/Feedback';
-
+import './SolutionDetailsPage.scss';
 const SolutionDetails: React.FC = () => {
   return (
     <>
       <div className="container-solution-details-page">
         <div className="title">
           <h1>Solution Details</h1>
+          {/* <ChallengeOverview/> */}
           <Action />
           <SolutitonDetails />
           <Feedback />
