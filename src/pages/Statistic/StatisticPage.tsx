c3544e0143661a0e81ed221a07d6f137e86a1a4f
Minh Tri <nguyenminhtri1808t@gmail.com>
2024-10-21 03:58:40 +0700

    feat(component): add section statistic component

--- src/pages/Statistic/StatisticPage.tsx
+++ src/pages/Statistic/StatisticPage.tsx
@@ -1,28 +1,63 @@
-import React from "react";
-import "./StatisticPage.scss"
-import { PreviewRecruiterChallenge, SolutionTable, StatisticCard } from "./partitals";
+import React from 'react';
+import './StatisticPage.scss';
+import { SectionStatistic } from './partitals';
+import { AcademicCapIcon, CommandLineIcon } from '@heroicons/react/24/outline';
+import ChallengeIncomplete from './partitals/ChallengeIncomplete/ChallengeIncomplete';
+import Solution from '../../components/common/Solution';
 
 const StatisticPage: React.FC = () => {
-    return (
-        <div className="statistic-container">
-            <h1 className='statistic-title'>Statistic Page</h1>
-
-            <div className="statistic-row">
-                <div className="statistic-left-column">
-                    <StatisticCard />
-                    <SolutionTable />
-                </div>
-
-                <div className="statistic-right-column">
-                    <PreviewRecruiterChallenge />
-                </div>
+  return (
+    <div className="statistic__page-container">
+      <h1 className="title-page">Statistic Page</h1>
+      <div className="content">
+        <div className="section-with-account">
+          <SectionStatistic
+            options
+            className="incomplete__challenge"
+            Icon={() => <AcademicCapIcon width={32} height={32} />}
+            title="Incomplete Challenges"
+          >
+            <div className="line"></div>
+            <div className="challenge__list">
+              {Array.from({
+                length: 6,
+              }).map(() => (
+                <ChallengeIncomplete
+                  className="challenge__incomplete"
+                  name="Link-sharing app"
+                  technicalList={['html', 'css']}
+                  difficulty="High"
+                  level="Diamon"
+                  score={125}
+                  imageURL="https://res.cloudinary.com/dz209s6jk/image/upload/f_auto,q_auto,w_700/Challenges/thypgk1nmxm4modj1wdl.jpg"
+                />
+              ))}
             </div>
-
-            <div className="challenge-component">
-                Challege Component
-            </div>
+          </SectionStatistic>
+          <div className="account"></div>
         </div>
-    )
+        <SectionStatistic
+          title="My Solution"
+          Icon={() => <CommandLineIcon width={24} height={24} />}
+          className="my__solution"
+        >
+          <div className="line"></div>
+          <div className="solution__list">
+            {Array.from({
+              length: 8,
+            }).map(() => (
+              <Solution
+                image="https://res.cloudinary.com/dz209s6jk/image/upload/f_auto,q_auto,w_700/Challenges/dmfvghaamqjt9lrx43ql.jpg"
+                time="About 5 hours ago"
+                name="Contact form"
+                tech={['Html', 'css', 'javascript']}
+              />
+            ))}
+          </div>
+        </SectionStatistic>
+      </div>
+    </div>
+  );
 };
 
 export default StatisticPage;
--- src/pages/Statistic/partitals/index.ts
+++ src/pages/Statistic/partitals/index.ts
@@ -1,3 +1,1 @@
-export { default as StatisticCard } from "./StatisticCard"
-export { default as SolutionTable } from "./SolutionTable"
-export { default as PreviewRecruiterChallenge } from "./PreviewRecruiterChallenge"
+export { default as SectionStatistic } from './SectionStatistic';
