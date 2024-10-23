9ebb7f1ee0b6af2b502a0ddd5883d71639d1fd36
Minh Tri <nguyenminhtri1808t@gmail.com>
2024-10-23 08:18:53 +0700

    feat(component): [FE-87-option-select-component] complete UI component

--- src/components/layout/app/partials/Header/partials/UserProfile/UserProfile.tsx
+++ src/components/layout/app/partials/Header/partials/UserProfile/UserProfile.tsx
@@ -1,45 +1,106 @@
-import React, { useState } from "react";
-import "./UserProfile.scss"
-import { NotificationIcon, UpAndDownIcon } from "../../../../../../../assets/icons";
-import { DefaultAvatar } from "../../../../../../../assets/images";
-import { useTranslation } from "react-i18next";
-import { Dropdown } from "./partials";
+import React, { useState } from 'react';
+import './UserProfile.scss';
+import {
+  NotificationIcon,
+  UpAndDownIcon,
+} from '../../../../../../../assets/icons';
+import { DefaultAvatar } from '../../../../../../../assets/images';
+import { useTranslation } from 'react-i18next';
+import { Dropdown } from './partials';
+import { OptionSelect } from '../../../../../../common';
+import { IOptionSelectItem } from '../../../../../../../types/entity/components';
+import { IOptionLanguage } from '../../../../../../../types/entity';
+import { toast } from 'react-toastify';
 
 const UserProfile: React.FC = () => {
-    const { t } = useTranslation();
-    const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+  const { i18n, t } = useTranslation();
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
 
-    const toggleDropdown = () => {
-        setIsDropdownOpen(prevState => !prevState);
-    };
+  const languageOptions: IOptionSelectItem[] = [
+    {
+      displayContent: `🇻🇳 ${t('Language.Vietnamese')}`,
+      optionValue: 'vi' as IOptionLanguage,
+    },
 
-    return (
-        <div className="user-profile-container">
-            <div className="notification-icon">
-                <NotificationIcon width={24} height={24} />
-            </div>
-            <div className="user-profile">
-                <div className="user-avatar">
-                    <img src={DefaultAvatar} alt={t('Layout.Header.UserProfile.avatar')} />
-                </div>
-                <div className="user-info">
-                    <div className="user-name">
-                        {t('Layout.Header.UserProfile.userName')}
-                    </div>
-                    <div className="user-id">
-                        #id
-                    </div>
-                </div>
-                <div
-                    className="drop-down"
-                    onClick={toggleDropdown}
-                >
-                    <UpAndDownIcon width={24} height={24} stroke="black" />
-                </div>
-            </div>
-            <Dropdown isOpen={isDropdownOpen} />
+    {
+      displayContent: `🇺🇸 ${t('Language.English')}`,
+      optionValue: 'en' as IOptionLanguage,
+    },
+  ];
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen((prevState) => !prevState);
+  };
+
+  const handleSetDefaultOptionLanguage: () => IOptionSelectItem = () => {
+    const language = i18n.language as IOptionLanguage;
+    if (language === 'vi') {
+      return languageOptions[0];
+    }
+
+    if (language === 'en') {
+      return languageOptions[1];
+    }
+
+    return languageOptions[1];
+  };
+
+  const selectOptionLanguage: (optionValue: string) => void = (optionValue) => {
+    const optionLanguageValue = optionValue as IOptionLanguage;
+    i18n.changeLanguage(optionLanguageValue);
+
+    if (optionLanguageValue === 'en') {
+      toast.success(
+        t('ChangeLanguage.success', {
+          language: t('Language.English'),
+        }),
+      );
+    }
+
+    if (optionLanguageValue === 'vi') {
+      toast.success(
+        t('ChangeLanguage.success', {
+          language: t('Language.Country'),
+        }),
+      );
+    }
+  };
+
+  return (
+    <div className="user-profile-container">
+      <div className="option">
+        <OptionSelect
+          style={{
+            width: '200px',
+          }}
+          handleSelect={selectOptionLanguage}
+          defaultOptionSelect={handleSetDefaultOptionLanguage()}
+          options={languageOptions}
+        />
+      </div>
+      <div className="notification-icon">
+        <NotificationIcon width={24} height={24} />
+      </div>
+      <div className="user-profile">
+        <div className="user-avatar">
+          <img
+            src={DefaultAvatar}
+            alt={t('Layout.Header.UserProfile.avatar')}
+          />
         </div>
-    );
+        <div className="user-info">
+          <div className="user-name">
+            {t('Layout.Header.UserProfile.userName')}
+          </div>
+          <div className="user-id">#id</div>
+        </div>
+        <div className="drop-down" onClick={toggleDropdown}>
+          <UpAndDownIcon width={24} height={24} stroke="black" />
+        </div>
+      </div>
+      <Dropdown isOpen={isDropdownOpen} />
+    </div>
+  );
 };
 
 export default UserProfile;
